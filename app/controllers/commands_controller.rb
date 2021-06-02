d7a03344ad8987a3429048b33ae2305702a9c53d
NUS Nick <nick@nustechnology.com>
2021-06-02 08:37:14 +0700

    Add comments

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,5 +1,8 @@
 class CommandsController < ApplicationController
 
+  # params example:
+  #   - {"robot"=>{"x"=>"", "y"=>"", "f"=>"", "isValid"=>"false"}, "commands"=>["PLACE 1,1,NORTH"]}
+  #   - {"robot"=>{"x"=>"1", "y"=>"1", "f"=>"NORTH", "isValid"=>"true"}, "commands"=>["MOVE"]}
   def run
     robot = service.run(params[:commands])
     render json: robot
--- app/services/movement_service.rb
+++ app/services/movement_service.rb
@@ -1,10 +1,12 @@
 class MovementService
   attr_reader :robot
 
+  # robot_params example: {"x"=>"1", "y"=>"1", "f"=>"NORTH"}
   def initialize robot_params
     @robot = Robot.new(robot_params)
   end
 
+  # commands example: ["MOVE"]
   def run commands
     commands.each do |command_str|
       robot.run_command(command_str)
